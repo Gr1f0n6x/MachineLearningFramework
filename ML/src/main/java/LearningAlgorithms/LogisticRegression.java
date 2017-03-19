c3b65280496a32f82b75d74a9c9ef2b154333f31
Gr1f0n6x <grifon52@gmail.com>
2017-03-19 19:04:30 +0300

    [ML 0.61] EJML 0.3 * Examples package
    1. Version of EJML was changed to 0.3
    2. Package with some examples was added

--- ML/src/main/java/LearningAlgorithms/LogisticRegression.java
+++ ML/src/main/java/LearningAlgorithms/LogisticRegression.java
@@ -7,6 +7,26 @@ import org.ejml.simple.SimpleMatrix;
  */
 public class LogisticRegression implements Model<SimpleMatrix> {
 
+    // cost = 1/m * sum[-Y' .* log(H) - (1 - Y)' .* log(1 - H)]
+    private double costFunction(SimpleMatrix H_predict, SimpleMatrix Y) {
+        // -Y'
+        SimpleMatrix Y1 = Y.negative().transpose();
+        //(1 - Y)'
+        //SimpleMatrix Y2 = Y.negative()
+        return 0;
+    }
+
+    // sigmoid
+    // 1 ./ (1 + exp(- X * Thetas))
+    private SimpleMatrix predictionFunction(SimpleMatrix X) {
+        return null;
+    }
+
+    // Qj = Qj - alpha / m * sum [(H - Y) .* Xj]
+    private double gradientDescent(SimpleMatrix X, SimpleMatrix Y, SimpleMatrix H_predict, int feature) {
+        return 0;
+    }
+
     @Override
     public void fit(SimpleMatrix X_train, SimpleMatrix Y_train, int epochNum, int batchSize) {
 
--- ML/src/main/java/Main.java
+++ ML/src/main/java/Main.java
@@ -18,42 +18,6 @@ import java.util.stream.IntStream;
 public class Main {
     public static void main(String[] args) {
 
-//        double[][] data = new double[][]{
-//                {1, 1, 1},
-//                {2, 4, 4},
-//                {3, 9, 9},
-//                {4, 16, 16},
-//                {5, 25, 25},
-//        };
-//
-//        SimpleMatrix matrix = new SimpleMatrix(data);
-//        SimpleMatrix X = DataSetUtilities.getTrainingSet(matrix, 0, 2);
-//
-//        SimpleMatrix Y = DataSetUtilities.getAnswersSet(matrix, 2);
-//
-//
-//        LinearRegression linearRegression = new LinearRegression(0.001, 100);
-//        linearRegression.fit(X, Y, 10, 0);
-//        linearRegression.plotCostFunctionHistory();
-//
-//        SimpleMatrix test = new SimpleMatrix(new double[][] {
-//                {1, 1},
-//                {2, 4},
-//                {3, 9},
-//                {4, 16},
-//                {5, 25}
-//        });
-//
-//        SimpleMatrix result = linearRegression.predict(test);
-//        DataSetUtilities.addColumns(test.extractVector(false, 0), result).print();
-//        DataSetUtilities.addColumns(X.extractVector(false, 0), Y).print();
-//
-//        XYLineChart lineChart = new XYLineChart("Prediction",
-//                new SimpleMatrix[] {DataSetUtilities.addColumns(test.extractVector(false, 0), result).transpose(),
-//                        DataSetUtilities.addColumns(X.extractVector(false, 0), Y).transpose()});
-//
-//        lineChart.plot();
-
         double[][] data = new double[][] {
                 {1, 4, 0},
                 {3, 2, 0},
@@ -94,5 +58,7 @@ public class Main {
 
         XYClassScatterPlot xyClassScatterPlot = new XYClassScatterPlot("demo", dataSet);
         xyClassScatterPlot.plot();
+
+
     }
 }
